021bdb758bef4ab7d2c6e822616c8a42a027a5fa
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-30 10:38:54 +0200

    refactor index.ts add key to mapped object

--- Projekty/elpassion/chatExample/my-app/pages/index.tsx
+++ Projekty/elpassion/chatExample/my-app/pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
       <h1 className={styles.uglyTitle}>Welcome in The Ugly Chat ;)</h1>
       <div className={styles.chatWindow}>
         <div className={styles.messageDisplay}>
-        {listOfMessage.length > 0 && listOfMessage.map(msg => <p>{msg}</p>)}
+        {listOfMessage.length > 0 && listOfMessage.map((msg, index) => <p key={index}>{msg}</p>)}
         </div>
         <input value={message} className={styles.inputText} onChange={(e) => handleMessage(e)} type='text' onKeyPress={(e) => {
           if(e.charCode == 13) {
