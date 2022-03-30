f962ad8320820c08c93f8262ceaadc69400fcc65
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-30 10:37:56 +0200

    sending to myself

--- Projekty/elpassion/chatExample/my-app/pages/index.tsx
+++ Projekty/elpassion/chatExample/my-app/pages/index.tsx
@@ -1,13 +1,31 @@
 import type { NextPage } from 'next'
+import { SetStateAction, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [message, setMessage] = useState('')
+  const [listOfMessage, setListOfMessage] = useState([])
+  const handleMessage = (e: { target: { value: SetStateAction<string> } }) => {
+    setMessage(e.target.value)
+  }
+  const addMessageToList = (msg: string) => {
+        setListOfMessage([...listOfMessage, msg])
+  }
+  
   return (
     <div className={styles.container}>
-      <h1>Welcome in The Ugly Chat ;)</h1>
-      <div className="chatWindow">
-        <div className="chatWindow__message-display"></div>
-        <input className="chatWindow__input" type='text'/>
+      <h1 className={styles.uglyTitle}>Welcome in The Ugly Chat ;)</h1>
+      <div className={styles.chatWindow}>
+        <div className={styles.messageDisplay}>
+        {listOfMessage.length > 0 && listOfMessage.map(msg => <p>{msg}</p>)}
+        </div>
+        <input value={message} className={styles.inputText} onChange={(e) => handleMessage(e)} type='text' onKeyPress={(e) => {
+          if(e.charCode == 13) {
+            addMessageToList(message)
+            setMessage('')
+          }            
+          }          
+          }/>
       </div>
     </div>
   )
